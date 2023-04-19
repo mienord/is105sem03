036abcfac5ef2ca0ba56f18284f4bc9713808b4e
mienord <mmnord@student.uia.no>
2023-04-19 22:14:09 +0200

    endra igjen

--- mycrypt/mycrypt.go
+++ mycrypt/mycrypt.go
@@ -2,14 +2,24 @@ package mycrypt
 
 var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøå0123456789.,:; ")
 
-func Krypter(melding []rune, alphabet []rune) []rune {
-	kryptertMelding := make([]rune, len(melding))
-	for i := 0; i < len(melding); i++ {
-		indeks := sokIAlfabetet(melding[i], alphabet)
-		kryptertMelding[i] = alphabet[indeks]
-	}
-	return kryptertMelding
+func Krypter(melding []rune, alphabet []rune, chiffer ...int) []rune {
+    c := 1 // default value
+    if len(chiffer) > 0 {
+        c = chiffer[0]
+    }
+
+    kryptertMelding := make([]rune, len(melding))
+    for i := 0; i < len(melding); i++ {
+        indeks := sokIAlfabetet(melding[i], alphabet)
+        if indeks+c >= len(alphabet) {
+            kryptertMelding[i] = alphabet[indeks+c-len(alphabet)]
+        } else {
+            kryptertMelding[i] = alphabet[indeks+c]
+        }
+    }
+    return kryptertMelding
 }
+
 
 
 func DeKrypter(kryptertMelding []rune, alphabet []rune, chiffer int) []rune {
