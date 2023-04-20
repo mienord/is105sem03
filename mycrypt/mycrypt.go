09812e71b0c2acfed8d1ea173f899a1b08e1b13d
mienord <mmnord@student.uia.no>
2023-04-20 17:38:55 +0200

    nytt

--- mycrypt/mycrypt.go
+++ mycrypt/mycrypt.go
@@ -2,22 +2,30 @@ package mycrypt
 
 var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøå0123456789.,:; ")
 
-func Krypter(melding []rune, alphabet []rune, chiffer ...int) []rune {
-    c := 1 // default value
-    if len(chiffer) > 0 {
-        c = chiffer[0]
-    }
+func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
+	kryptertMelding := make([]rune, len(melding))
+	for i := 0; i < len(melding); i++ {
+		indeks := sokIAlfabetet(melding[i], alphabet)
+		if indeks+chiffer >= len(alphabet) {
+			kryptertMelding[i] = alphabet[indeks+chiffer-len(alphabet)]
+		} else {
+			kryptertMelding[i] = alphabet[indeks+chiffer]
+		}
+	}
+	return kryptertMelding
+}
 
-    kryptertMelding := make([]rune, len(melding))
-    for i := 0; i < len(melding); i++ {
-        indeks := sokIAlfabetet(melding[i], alphabet)
-        if indeks+c >= len(alphabet) {
-            kryptertMelding[i] = alphabet[indeks+c-len(alphabet)]
-        } else {
-            kryptertMelding[i] = alphabet[indeks+c]
-        }
-    }
-    return kryptertMelding
+func DeKrypter(kryptertMelding []rune, alphabet []rune, chiffer int) []rune {
+	melding := make([]rune, len(kryptertMelding))
+	for i := 0; i < len(kryptertMelding); i++ {
+		indeks := sokIAlfabetet(kryptertMelding[i], alphabet)
+		if indeks-chiffer < 0 {
+			melding[i] = alphabet[indeks-chiffer+len(alphabet)]
+		} else {
+			melding[i] = alphabet[indeks-chiffer]
+		}
+	}
+	return melding
 }
 
 func sokIAlfabetet(symbol rune, alfabet []rune) int {
